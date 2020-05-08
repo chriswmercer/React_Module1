ab6662b86278136a2e168567bdf5b3db84676631
Chris Mercer <chriswm@me.com>
2020-05-08 21:26:19 +0100

    Module 1.4-1.7

--- fundamentals-prod/src/index.js
+++ fundamentals-prod/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const root = document.getElementById('root')
-const container = React.createElement('div', {}, "Hello React");
+const container = React.createElement(
+  'div',
+  {},
+  "Hello React",
+  "Further child",
+  "I am learning react",
+  React.createElement(
+    'a',
+    { href: "#"},
+    'Learn React'),
+    "Hello World",
+    React.createElement(
+      'div',
+      {},
+      React.createElement(
+        "p",
+        {},
+        "I am a paragraph")));
+
+console.log(container);
 
 render(container, root);
 
